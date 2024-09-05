375520dc85da36d9c211234634cef456df2f9c5d
Mustafa Baker <s235092@dtu.dk>
2024-09-05 16:40:22 +0200

    Cleaned up redundancy in code

--- io.c
+++ io.c
@@ -1,4 +1,3 @@
-
 #include <errno.h>
 #include <unistd.h>
 
@@ -7,86 +6,55 @@
 /* Reads next char from stdin. If no more characters, it returns EOF */
 int
 read_char() {
-    char arr[1];
-    int bytes_read = read(0,arr,1);
-    if(bytes_read <= 0) {
-        return EOF;
-    }
-  return arr[0];
+    char c;
+    return (read(0, &c, 1) <= 0) ? EOF : c;
 }
 
 /* Writes c to stdout.  If no errors occur, it returns 0, otherwise EOF */
 int
 write_char(char c) {
-    char arr[1] = {c};
-    int bytes_written = write(1,arr,1);
-    if(bytes_written <= 0) {
-        return EOF;
-    }
-  return arr[0];
+    return (write(1, &c, 1) <= 0) ? EOF : 0;
 }
 
 /* Writes a null-terminated string to stdout.  If no errors occur, it returns 0, otherwise EOF */
 int
 write_string(char* s) {
-    int inital_buff = 100;
-    char buffer[inital_buff];
-    int index = 0;
-    char c;
-
-    while ((c = s[index]) != '\0' && index < inital_buff -1) {
-        buffer[index] = c;
-        index++;
-    }
-    buffer[index] = '\0';
-    int bytes_written = write(1, buffer, index);
-    if(bytes_written < index) return EOF;
-  return 0;
+    int len = 0;
+    while (s[len] != '\0') len++;
+    return (write(1, s, len) < len) ? EOF : 0;
 }
 
-/* Writes n to stdout (without any formatting).   
+/* Writes n to stdout (without any formatting).
  * If no errors occur, it returns 0, otherwise EOF
  */
 int
 write_int(int n) {
     char buffer[12];
-    int i = 0;
-    int isNegative = 0;
+    int i = 0, isNegative = 0;
 
-    if(n == 0) {
+    if (n == 0) {
         buffer[i++] = '0';
-        buffer[i] = '\0';
     } else {
-        if(n < 0) {
+        if (n < 0) {
             isNegative = 1;
             n = -n;
         }
 
-        while(n != 0) {
-            int digit = n % 10;
-            buffer[i++] = digit + '0';
-            n = n / 10;
+        while (n != 0) {
+            buffer[i++] = (n % 10) + '0';
+            n /= 10;
         }
 
-        if(isNegative) {
+        if (isNegative) {
             buffer[i++] = '-';
         }
 
-        buffer[i] = '\0';
-
-        int start = 0;
-        int end = i - 1;
-        while (start < end) {
+        for (int start = 0, end = i - 1; start < end; start++, end--) {
             char temp = buffer[start];
             buffer[start] = buffer[end];
             buffer[end] = temp;
-            start++;
-            end--;
         }
     }
-    int bytes_written = write(1, buffer, i);
-    if(bytes_written < i) {
-        return EOF;
-    }
-  return 0;
+
+    return (write(1, buffer, i) < i) ? EOF : 0;
 }
