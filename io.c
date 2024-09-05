e0eaff0de619d272815dbfcb472f09892bc5eb7b
Frebbers <s224804@dtu.dk>
2024-09-05 16:19:05 +0200

    finished read char + write char/string

--- io.c
+++ io.c
@@ -7,20 +7,40 @@
 /* Reads next char from stdin. If no more characters, it returns EOF */
 int
 read_char() {
-  return EOF;
+  char buf[1];
+  int bytes_read = read(0, buf, 1);
+  if (bytes_read <= 0) {
+    return EOF;
+  }
+  return buf[0];
 }
 
 /* Writes c to stdout.  If no errors occur, it returns 0, otherwise EOF */
 int
 write_char(char c) {
+    if (write(1, &c, 1) == 1) {
+        return 0;
+    }
   return EOF;
 }
 
 /* Writes a null-terminated string to stdout.  If no errors occur, it returns 0, otherwise EOF */
 int
 write_string(char* s) {
-  return EOF;
-}
+    int i = 0;
+    int result = 0;
+    while (s[i] != '\0') {
+        result = write_char(s[i]);
+        if (result == EOF) {
+            return EOF;
+        }
+        i++;
+    }
+        return 0;
+    }
+
+
+
 
 /* Writes n to stdout (without any formatting).   
  * If no errors occur, it returns 0, otherwise EOF
