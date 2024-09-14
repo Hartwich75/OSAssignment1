b50a7131e4b210c3b90e75ba2e83a712b64d2e81
Mustafa Baker <s235092@dtu.dk>
2024-09-14 20:03:23 +0200

    Created 2 testcases as a test :D

--- test.sh
+++ test.sh
@@ -7,4 +7,14 @@ out="0,3,5;"
 
 [[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
 
+in2="aabbcaaq"
+out2="0,1,5,6"
+
+[[ $(./cmd_int <<< "$in2") == "$out2"* ]] && echo "PASSED" || echo "FAILED"
+
+in3=ababababccccq
+out2="0,2,4,6"
+
+[[ $(./cmd_int <<< "$in3") == "$out3"* ]] && echo "PASSED" || echo "FAILED"
+
 
