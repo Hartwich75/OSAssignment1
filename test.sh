2a1cd6bca284a56ced4de078154b4d15a6b66e75
Mustafa Baker <s235092@dtu.dk>
2024-09-15 12:34:57 +0200

    Added more tests

--- test.sh
+++ test.sh
@@ -12,9 +12,17 @@ out2="0,1,5,6;"
 
 [[ $(./cmd_int <<< "$in2") == "$out2"* ]] && echo "PASSED" || echo "FAILED"
 
-in3="ababababccccq"
-out2="0,2,4,6;"
+in3="abababababababababababababaccccccccccccccccq"
+out3="0,2,4,6,8,10,12,14,16,18,20,22,24,26;"
 
 [[ $(./cmd_int <<< "$in3") == "$out3"* ]] && echo "PASSED" || echo "FAILED"
 
+in4="sadccccaq"
+out4="0,5;"
 
+[[ $(./cmd_int <<< "$in4") == "$out4"* ]] && echo "PASSED" || echo "FAILED"
+
+in5="abababababcccccccqls"
+out5="0,2,4,6,8;"
+
+[[ $(./cmd_int <<< "$in5") == "$out5"* ]] && echo "PASSED" || echo "FAILED"
