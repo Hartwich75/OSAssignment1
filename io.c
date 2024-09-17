5bcaa6d0230c9ed087d08893774c70e10d50da8e
s224804 <frederik.bode@hendrichsen.dk>
2024-09-17 10:36:02 +0200

    replaced io.c with replacement implementation

--- io.c
+++ io.c
@@ -1,28 +1,33 @@
 
 #include <errno.h>
 #include <unistd.h>
+#include <stdio.h>
 
 #include "io.h"
+
+/* 
+ * This implementation cheats by using stdio
+ * 
+ */
 
 /* Reads next char from stdin. If no more characters, it returns EOF */
 int
 read_char() {
-    char c;
-    return (read(0, &c, 1) <= 0) ? EOF : c;
+  return getchar();
 }
 
 /* Writes c to stdout.  If no errors occur, it returns 0, otherwise EOF */
 int
 write_char(char c) {
-    return (write(1, &c, 1) <= 0) ? EOF : 0;
+  int r = putchar(c); 
+  return (r > 0 ? 0 : EOF); 
 }
 
 /* Writes a null-terminated string to stdout.  If no errors occur, it returns 0, otherwise EOF */
 int
 write_string(char* s) {
-    int len = 0;
-    while (s[len] != '\0') len++;
-    return (write(1, s, len) < len) ? EOF : 0;
+  int r = puts(s); 
+  return (r > 0 ? 0 : EOF); 
 }
 
 /* Writes n to stdout (without any formatting).   
@@ -30,17 +35,6 @@ write_string(char* s) {
  */
 int
 write_int(int n) {
-    char c;
-
-    if (n < 0) {
-        if (write(1, "-", 1) != 1) return EOF;
-        n = -n;
-    }
-
-    if (n >= 10) {
-        if (write_int(n / 10) == EOF) return EOF;
-    }
-
-    c = (n % 10) + '0';
-    return (write(1, &c, 1) == 1) ? 0 : EOF;
+  int r = printf("%d", n);
+  return (r > 0 ? 0 : EOF); 
 }
